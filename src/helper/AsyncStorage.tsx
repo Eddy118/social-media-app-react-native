8fd5b3b011a3fc6e586cbccb1b61ec0c7b38f33a
Eddy118 <42584137+Eddy118@users.noreply.github.com>
2025-05-18 00:42:44 +0100

    local storage added

---
+++ src/helper/AsyncStorage.tsx
@@ -0,0 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function setItemByKey(key: string, value: any) {
+    try {
+        const jsonValue = JSON.stringify(value);
+        await AsyncStorage.setItem(key, jsonValue);
+    } catch (e) {
+        console.log('e', e);
+    }
+}
+
+export async function getItemByKey(key: string) {
+    try {
+        const jsonValue = await AsyncStorage.getItem(key);
+        return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+        console.log('e', e);
+        return null;
+    }
+}
+
+export function removeItemByKey(key: string) {
+    try {
+        AsyncStorage.removeItem(key);
+    } catch (e) {
+        console.log('e', e);
+    }
+}
